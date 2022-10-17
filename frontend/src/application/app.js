f5af20c62c83978fd464583bded6e06477325c49
Francisco Macedo <franciscovcbm@gmail.com>
2022-10-17 17:26:50 +0100

    Add frontend directory

---
+++ frontend/src/application/app.js
@@ -0,0 +1,7 @@
+// This is the scss entry file
+import "../styles/index.scss";
+import M from "materialize-css/dist/js/materialize.min";
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  M.AutoInit();
+});
--- sf_django_template/settings.py
+++ sf_django_template/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_boilerplate'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend/build",
+]
+
+WEBPACK_LOADER = {
+    'MANIFEST_FILE': BASE_DIR / "frontend/build/manifest.json",
+}
